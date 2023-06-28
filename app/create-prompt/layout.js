900cd520792678f19f70c5ef2896f8090d3c473f
Shippu Kumar <Itzshippu@gmail.com>
2023-06-28 22:02:01 +0530

    fdsg

---
+++ app/create-prompt/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: "Create Prompt",
+    description: "create prompt page"
+}
+
+const RootLayout = ({ children }) => {
+    return (
+        <>
+            {children}
+        </>
+    )
+}
+
+export default RootLayout
